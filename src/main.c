50b493c5295b735aa1d01a37f2808a5b95c9fe1f
Torsten Wunderlich <wunderlich@actualchange.eu>
2014-04-15 08:46:56 -0700

    Fully functional

--- src/main.c
+++ src/main.c
@@ -19,11 +19,12 @@ void draw_hours_top(Layer *layer, GContext *ctx);
 void draw_hours_bottom(Layer *layer, GContext *ctx);
 void draw_minutes_top(Layer *layer, GContext *ctx);
 void draw_minutes_bottom(Layer *layer, GContext *ctx);
-void draw_bar(Layer *layer, GContext *ctx, int16_t on, int16_t max);
+void draw_bar(Layer *layer, GContext *ctx, int16_t on, int16_t max); //convenience function
+void draw_bar_blank(Layer *layer, GContext *ctx, int16_t on, int16_t max, int16_t leaveblank);
+Layer *create_and_add_layer(Layer *parent, GRect rect);
 
 void handle_init(void) {
   init_window();
-  //setup the handlers
   tick_timer_service_subscribe(SECOND_UNIT, handle_ticks);
 }
 
@@ -33,20 +34,13 @@ void init_window(void) {
   Layer *root_layer = window_get_root_layer(my_window);
   //get geometry
   GRect bounds = layer_get_frame(root_layer);
-  int16_t layer_hight = bounds.size.h/3; //devide the screen in thirds
-  int16_t layer_width = bounds.size.w;
-  
-  seconds_layer        = layer_create(GRect(0, 0, layer_width, 32));
-  hours_layer_top      = layer_create(GRect(0, 33, layer_width, 32));
-  hours_layer_bottom   = layer_create(GRect(0, 66, layer_width, 32));
-  minutes_layer_top    = layer_create(GRect(0, 99, layer_width, 32));
-  minutes_layer_bottom = layer_create(GRect(0, 132, layer_width, 32));
-  //put layers into root layer
-  layer_add_child(root_layer, seconds_layer);
-  layer_add_child(root_layer, hours_layer_top);
-  layer_add_child(root_layer, hours_layer_bottom);
-  layer_add_child(root_layer, minutes_layer_top);
-  layer_add_child(root_layer, minutes_layer_bottom);
+  int16_t layer_width = bounds.size.w;  
+  //create_and_add_layer(root_layer, seconds_layer, GRect(0, 33, layer_width, 32));
+  seconds_layer        = create_and_add_layer(root_layer, GRect(0,   0, layer_width, 32));
+  hours_layer_top      = create_and_add_layer(root_layer, GRect(0,  33, layer_width, 32));
+  hours_layer_bottom   = create_and_add_layer(root_layer, GRect(0,  66, layer_width, 32));
+  minutes_layer_top    = create_and_add_layer(root_layer, GRect(0,  99, layer_width, 32));
+  minutes_layer_bottom = create_and_add_layer(root_layer, GRect(0, 132, layer_width, 32));
   //add redraw handlers
   layer_set_update_proc(seconds_layer, draw_seconds);
   layer_set_update_proc(hours_layer_top, draw_hours_top);
@@ -56,6 +50,7 @@ void init_window(void) {
 }
 
 void handle_deinit(void) {
+  tick_timer_service_unsubscribe();
   layer_destroy(minutes_layer_bottom);
   layer_destroy(minutes_layer_top);
   layer_destroy(hours_layer_bottom);
@@ -68,24 +63,29 @@ void handle_ticks(struct tm *tick_time, TimeUnits units_changed) {
   //I think there is more in the unit_changed parameter. Maybe I don't need to invalidate all layers.
   now = *tick_time;
   layer_mark_dirty(seconds_layer);
-  layer_mark_dirty(hours_layer_top);  
-  layer_mark_dirty(hours_layer_bottom);  
+  layer_mark_dirty(hours_layer_top);
+  layer_mark_dirty(hours_layer_bottom);    
   layer_mark_dirty(minutes_layer_top);
   layer_mark_dirty(minutes_layer_bottom);
+}
+
+Layer *create_and_add_layer(Layer *parent, GRect rect) {
+  Layer *new_layer = layer_create(rect); //allocs memory
+  layer_add_child(parent, new_layer);
+  return new_layer;
 }
 
 void draw_seconds(Layer *layer, GContext *ctx) {
   int16_t ticktock = now.tm_sec % 2;
   GColor color = ticktock ? GColorBlack : GColorWhite;
   GRect bounds = layer_get_bounds(layer);
-  // Draw a black filled rectangle
   graphics_context_set_fill_color(ctx, color);
   graphics_fill_circle(ctx, GPoint(bounds.size.w/2, bounds.size.h/2), (bounds.size.h/2)-2);
 }
 
 void draw_minutes_top(Layer *layer, GContext *ctx) {
   int16_t toprow    = now.tm_min/5;
-  draw_bar(layer, ctx, toprow, 11);
+  draw_bar_blank(layer, ctx, toprow, 11, 3);
 }
 
 void draw_minutes_bottom(Layer *layer, GContext *ctx) {
@@ -104,15 +104,27 @@ void draw_hours_bottom(Layer *layer, GContext *ctx) {
 }
 
 void draw_bar(Layer *layer, GContext *ctx, int16_t on, int16_t max) {
+  //no blanks please
+  draw_bar_blank(layer, ctx, on, max, 0);
+}
+
+void draw_bar_blank(Layer *layer, GContext *ctx, int16_t on, int16_t max, int16_t leaveblank) {
   GRect bounds = layer_get_bounds(layer);
   const int16_t width = bounds.size.w/max;
+  const int16_t hight = bounds.size.h-2;
   //delete the layer
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, bounds, 0, GCornerNone);
   //and draw the shit
   graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
   for(int i=0; i<on; i++) {
-    graphics_fill_rect(ctx, GRect((width*i)+2,2,width-2,bounds.size.h), 5, GCornersAll);
+    //0, 1, 2, 3, 4, 5, 6, 7
+    if (!((i+1)%leaveblank)) {
+      graphics_draw_round_rect(ctx, GRect((width*i)+2,2,width-2,hight), 5);
+    } else {
+      graphics_fill_rect(ctx, GRect((width*i)+2,2,width-2,hight), 5, GCornersAll);    
+    }
   }
 }
 
