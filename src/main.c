fa72e4e02477116af1eb8db040cd87cf7b3b1555
Torsten Wunderlich <wunderlich@actualchange.eu>
2014-04-25 15:05:08 -0700

    now with splash screen

--- src/main.c
+++ src/main.c
@@ -7,12 +7,18 @@ Layer *hours_layer_top;
 Layer *hours_layer_bottom;
 Layer *minutes_layer_top;
 Layer *minutes_layer_bottom;
+//splash screen
+Layer *splash_root;
+BitmapLayer *splash_layer;
+GBitmap *splash_bitmap;
+
 struct tm now; //obj-c is so beautiful... this is the only chance to retain time throughout all draws.
 
 //forward declaration as learned in university :-)
-void handle_init(void);
-void init_window(void);
-void handle_deinint(void);
+void handle_init();
+void init_window();
+void init_watch();
+void handle_deinit();
 void handle_ticks(struct tm *tick_time, TimeUnits units_changed);
 void draw_seconds(Layer *layer, GContext *ctx);
 void draw_hours_top(Layer *layer, GContext *ctx);
@@ -23,10 +29,8 @@ void draw_bar(Layer *layer, GContext *ctx, int16_t on, int16_t max); //convenien
 void draw_bar_blank(Layer *layer, GContext *ctx, int16_t on, int16_t max, int16_t fullevery);
 Layer *create_and_add_layer(Layer *parent, GRect rect);
 
-void handle_init(void) {
-  init_window();
-  tick_timer_service_subscribe(SECOND_UNIT, handle_ticks);
-}
+void display_splash_layer(Window *window);
+void destroy_splash_layer();
 
 #define BLACKONWHITE
 #ifdef BLACKONWHITE
@@ -36,14 +40,33 @@ void handle_init(void) {
   #define FOREGROUND GColorWhite
   #define BACKGROUND GColorBlack  
 #endif
-void init_window(void) {
+
+int main(void) {
+  handle_init();
+  app_event_loop();
+  handle_deinit();
+}
+
+void handle_init() {
+  init_window();
+  display_splash_layer(my_window);
+  app_timer_register(2000, init_watch, NULL);
+}
+
+void init_window() {
   my_window = window_create();
   window_set_background_color(my_window, BACKGROUND);
   window_stack_push(my_window, true);
+}
+
+void init_watch() {
+  //get rid of splash_screen
+  destroy_splash_layer();
+  
   Layer *root_layer = window_get_root_layer(my_window);
   //get geometry
   GRect bounds = layer_get_frame(root_layer);
-  int16_t layer_width = bounds.size.w;  
+  int16_t layer_width = bounds.size.w;
   //create_and_add_layer(root_layer, seconds_layer, GRect(0, 33, layer_width, 32));
   seconds_layer        = create_and_add_layer(root_layer, GRect(0,   0, layer_width, 32));
   hours_layer_top      = create_and_add_layer(root_layer, GRect(0,  33, layer_width, 32));
@@ -56,9 +79,26 @@ void init_window(void) {
   layer_set_update_proc(hours_layer_bottom, draw_hours_bottom);
   layer_set_update_proc(minutes_layer_top, draw_minutes_top);
   layer_set_update_proc(minutes_layer_bottom, draw_minutes_bottom);
+  //hook up to the service
+  tick_timer_service_subscribe(SECOND_UNIT, handle_ticks);
 }
 
-void handle_deinit(void) {
+void display_splash_layer(Window *window) {
+  Layer *root_layer = window_get_root_layer(window);
+  splash_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BERLIN_BEAR);
+  splash_layer = bitmap_layer_create(layer_get_frame(root_layer));
+  bitmap_layer_set_bitmap(splash_layer, splash_bitmap);
+  splash_root = bitmap_layer_get_layer(splash_layer);
+  layer_add_child(root_layer, splash_root);
+}
+
+void destroy_splash_layer() {
+  layer_remove_from_parent(splash_root);
+  bitmap_layer_destroy(splash_layer);
+  gbitmap_destroy(splash_bitmap);
+}
+
+void handle_deinit() {
   tick_timer_service_unsubscribe();
   layer_destroy(minutes_layer_bottom);
   layer_destroy(minutes_layer_top);
@@ -146,10 +186,4 @@ void draw_bar_blank(Layer *layer, GContext *ctx, int16_t on, int16_t max, int16_
       graphics_fill_rect(ctx, GRect((width*i)+PADDING,SHORTBAR,paddedwidth,paddedhight), 5, GCornersAll);    
     }
   }
-}
-
-int main(void) {
-  handle_init();
-  app_event_loop();
-  handle_deinit();
 }
